5e016a35c0101460ced2f47e7711259a0d36ed57
AndrewAMD <30497817+AndrewAMD@users.noreply.github.com>
2018-06-18 23:03:02 -0500

    Update libdtc.h

--- libdtc/libdtc.h
+++ libdtc/libdtc.h
@@ -9,7 +9,35 @@
 // This type of a setup is perfect for triggering timers to interrupt sleep.
 
 // This library allows as many simultaneous connections as desired.  You can track any given connection with a handler.
+// Also, you can count the number of active connections.
+// Most DTC servers will supply one or two ports to cover all functionality.  Sierra Chart's built-in server has two ports.
 
 // This library promotes asynchronous usage by returning immediately unless a block limit is supplied.
 // Note that a "block limit" is not to be confused with a "timeout".
 // For example, in Zorro, a healthy Broker Plugin will run BrokerProgress() every ~10ms or so in order to keep the menu buttons alive on the GUI.
+
+typedef struct DTC_CONNECTION_CONFIG
+{
+  char host[32];
+  char port[32];
+  bool tls_enabled;
+  // TO DO
+} DTC_CONNECTION_CONFIG;
+
+// immediately returns handler of new connection.
+// Connection task is assigned to background thread.
+// Returns handler to this new connection.
+uint32_t new_connection(DTC_CONNECTION_CONFIG* config);
+
+enum DTC_CONNECTION_STATUS
+{
+  status_please_wait = 0,
+  status_connection_failed =1,
+  status_handshake_failed = 2,
+  status_login_failed = 3,
+  status_login_successful = 4,
+  // TO DO
+}
+
+// immediately returns the status of the connection.
+DTC_CONNECTION_STATUS check_connection(uint32_t handler);
