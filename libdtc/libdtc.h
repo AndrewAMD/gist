73256eb41a25721e774096ead9e3d04537cf5747
AndrewAMD <30497817+AndrewAMD@users.noreply.github.com>
2018-06-19 10:25:22 -0500

    Update libdtc.h

--- libdtc/libdtc.h
+++ libdtc/libdtc.h
@@ -49,4 +49,13 @@ LDTC_STATUS ldtc_status(LDTC_HANDLE h, int limit_ms);
 // Returns a (temporary) pointer to the next available message or NULL if none available.
 // This will pop the first message from the front of the message queue.
 // Once this function is called again, the previous pointer will be invalid.
-const char* ldtc_message(LDTC_HANDLE h);
+//const char* ldtc_message(LDTC_HANDLE h);
+
+#define LDTC_MESSAGE_LENGTH 256
+
+// Writes a message to the input char array of size LDTC_MESSAGE_LENGTH.
+// This will pop the first message from the front of the message queue.
+// Returns the number of messages remaining on the queue, or -1 if there was an error.
+int ldtc_message(LDTC_HANDLE h, char* msg);
+
+
