c4aff9ad7f2ea4585bcfd6a2b03de4facdfd16b5
AndrewAMD <30497817+AndrewAMD@users.noreply.github.com>
2018-06-22 13:44:53 -0500

    Update getfromthread.cpp

--- asio/getfromthread.cpp
+++ asio/getfromthread.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <thread>
-#include "post_function_use_future.hpp"
+#include <functional>
+#include "use_future_helpers.hpp"
 
 namespace asio = boost::asio;
 
 int SayHello()
 {
 	std::cout << "Hello!" << std::endl;
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 	return 0;
 }
 
@@ -14,6 +16,7 @@ template <typename T>
 T GetSum(T a, T b)
 {
 	std::cout << "Adding " << a << " and " << b << std::endl;
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 	return a + b;
 }
 
@@ -23,12 +26,25 @@ int main() {
 
 	std::thread t{ [&] { io.run(); } };
 		
-	auto res1 = post_function_use_future(io, SayHello);
+	auto res1 = post_use_future(io, SayHello);
 	res1.get(); // block until return value received.
 	
-	auto res2 = post_function_use_future(io, []() {return  GetSum(20, 14); });
+	auto res2 = post_use_future(io, []() {return  GetSum(20, 14); });
 	std::cout << res2.get() << std::endl; // block until return value received.
+
+	auto res3 = post_use_future(io, (std::function <int()> )std::bind(&SayHello));
+	res3.get(); // block until return value received.
 	
+	asio::steady_timer timer(io);
+	timer.expires_from_now(std::chrono::milliseconds(1000));
+	auto res4 = async_wait_use_future(timer, [](const boost::system::error_code& ec)
+	{
+		return (int)ec.value();
+	});
+	std::this_thread::sleep_for(std::chrono::milliseconds(500));
+	timer.expires_at(std::chrono::steady_clock::now());
+	std::cout << res4.get() << std::endl; // block until return value received.
+
 	wg.reset();
 	if(t.joinable()) t.join();
 	
