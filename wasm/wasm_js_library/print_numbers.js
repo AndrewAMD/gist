1223248e9f2d74763175c5a75689ed7094739f1b
Andrew Dolder <andrew@vitaltrades.com>
2019-01-24 20:43:40 -0600

    removed unrelated comment

--- wasm/wasm_js_library/print_numbers.js
+++ wasm/wasm_js_library/print_numbers.js
@@ -1,7 +1,7 @@
  var LibraryPrintNumbers = {
-  // These are empty, designed to be replaced when a debugger is invoked
+  
   print_numbers: function(a,b,c) {
-	console.log(a);
+    console.log(a);
     console.log(b);
     console.log(c);
   },
